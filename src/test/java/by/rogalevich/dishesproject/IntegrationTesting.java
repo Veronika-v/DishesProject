739a2b5bf7521ab604defc2b233d7d659b589963
Veronika Rogalevich <comedi.nika@mail.ru>
2020-12-18 02:21:42 +0300

    integration tests

---
+++ src/test/java/by/rogalevich/dishesproject/IntegrationTesting.java
@@ -0,0 +1,62 @@
+package by.rogalevich.dishesproject;
+
+
+import by.rogalevich.dishesproject.controller.MainController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTesting {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test() throws Exception{
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("")));
+    }
+
+    @Test
+    public void accessLoginTest() throws Exception{
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    public void correctLoginTest() throws Exception{
+        this.mockMvc.perform(formLogin().user("u").password("p"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+    }
+
+    @Test
+    public void badCredentials() throws Exception{
+        this.mockMvc.perform(post("/login").param("user","Alfred"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+    }
+
+}
